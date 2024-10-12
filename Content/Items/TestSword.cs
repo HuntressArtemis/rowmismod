c523174d3eb8579fe3c621a21cc715301b3b69c0
Artemis <erisartemisnarcissus@gmail.com>
2024-10-12 17:07:05 +0200

    test

--- Content/Items/TestSword.cs
+++ Content/Items/TestSword.cs
@@ -12,7 +12,7 @@ namespace rowmismod.Content.Items
 		// The Display Name and Tooltip of this item can be edited in the 'Localization/en-US_Mods.rowmismod.hjson' file.
 		public override void SetDefaults()
 		{
-			Item.damage = 50;
+			Item.damage = 150;
 			Item.DamageType = DamageClass.Melee;
 			Item.width = 40;
 			Item.height = 40;
