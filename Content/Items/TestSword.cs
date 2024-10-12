5ac089a33ecf9fb41c946b908795e6d2610963e0
Rowan <rowan242115@gmail.com>
2024-10-12 16:18:49 +0100

    testaaaa

--- Content/Items/TestSword.cs
+++ Content/Items/TestSword.cs
@@ -12,7 +12,7 @@ namespace rowmismod.Content.Items
 		// The Display Name and Tooltip of this item can be edited in the 'Localization/en-US_Mods.rowmismod.hjson' file.
 		public override void SetDefaults()
 		{
-			Item.damage = 2000;
+			Item.damage = 1000;
 			Item.DamageType = DamageClass.Melee;
 			Item.width = 50;
 			Item.height = 40;
